6ae95eceb79fd3c9d1062d459f2df8298af1811c
mdhcodes <mdhcodes@gmail.com>
2024-04-26 22:12:44 -0400

    Add new_post function in views.py

--- network/forms.py
+++ network/forms.py
@@ -11,9 +11,15 @@ class CreatePostForm(forms.ModelForm):
     # Specify the name of the model to use.
     class Meta:
         model = Post
-        fields = "__all__"
-        widgets = {
-            "likes": forms.HiddenInput(),
-            "created": forms.HiddenInput(),
-            "author": forms.HiddenInput()
+        fields = '__all__'
+        # https://stackoverflow.com/questions/5827590/css-styling-in-django-forms
+        # https://medium.com/swlh/how-to-style-your-django-forms-7e8463aae4fa
+        # https://docs.djangoproject.com/en/5.0/ref/forms/widgets/
+        # Style Django forms with built-in widgets.
+        widgets = {            
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'post': forms.Textarea(attrs={'class': 'form-control'}),
+            'likes': forms.HiddenInput(),
+            'created': forms.HiddenInput(),
+            'author': forms.HiddenInput()
         }
--- network/views.py
+++ network/views.py
@@ -4,11 +4,25 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Post
+
+from .forms import CreatePostForm
 
 
 def index(request):
-    return render(request, "network/index.html")
+
+    """
+    All Posts: The “All Posts” link in the navigation bar should take the user to a page where they can see all posts from all users, with the most recent posts first. 
+    """
+    
+    # Include the “New Post” form before “All Posts” on index.html. 
+    form = CreatePostForm()
+
+    context = {
+        "form": form
+    }
+
+    return render(request, "network/index.html", context)
 
 
 def login_view(request):
@@ -61,3 +75,30 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+    
+
+"""
+New Post: Authenticated users may write a new text-based post. 
+""" 
+
+def new_post(request):
+    # POST request
+    if request.method == 'POST':
+        newPost = CreatePostForm(request.POST)
+
+        user_name = request.user
+        print('User:', user_name)
+
+        title = newPost['title'].value()
+        post = newPost['post'].value()
+
+        newPost = Post(            
+            title=title,
+            post=post,
+            likes=0,
+            author=user_name
+        )
+
+        newPost.save()
+
+        return HttpResponseRedirect(reverse('index'))
